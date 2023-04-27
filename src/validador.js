7edaa9c6449a745ceaa65db10bb5ac4d45f279b8
webacademy <lauralimaufam@gmail.com>
2023-04-27 19:58:57 -0400

    json validatores part 1

--- src/validador.js
+++ src/validador.js
@@ -1,22 +1,21 @@
 // Conjunto de REGEX para validação de entradas.
 
-function validar_email(email)
-{
-  // escreva seu código aqui
- 
-}
-  
-function validar_cep(cep){
-  // escreva seu código aqui
-
+function validar_email(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
 }
 
-function validar_cpf(cpf){
-// escreva seu código aqui
-
+function validar_cep(cep) {
+  var regex = /^[0-9]{5}-[0-9]{3}$/;
+  return regex.test(cep);
 }
 
-function validar_telefone(telefone){
+function validar_cpf(cpf) {
+  var regex = /^(\d)\1{10}$/;
+  return regex.test(cpf);
+}
+
+function validar_telefone(telefone) {
   // escreve seu código aqui
 
 }
