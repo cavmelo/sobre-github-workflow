ed6145a0baa0b4c77a667d27ae0defde93745b57
eduardo <duaraujo26@gmail.com>
2023-04-27 20:13:34 -0400

    adicionando novos validadores

--- src/validador.js
+++ src/validador.js
@@ -1,18 +1,33 @@
 // Conjunto de REGEX para validação de entradas.
 
-function validar_email(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
+function validar_email(email)
+{
+  let validarRegExNoEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.match(validarRegExNoEmail)) {
+    return true; 
+  } else {
+    return false; 
+  }
+ 
+}
+  
+function validar_cep(cep){
+  let regex = /^[0-9]{5}-[0-9]{3}$/;
+  if(regex.test(cep)) {
+    return true;
+  } else {
+    return false
+  }
 }
 
-function validar_cep(cep) {
-  var regex = /^[0-9]{5}-[0-9]{3}$/;
-  return regex.test(cep);
-}
+function validar_cpf(cpf){
+  let regex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+  if(regex.test(cpf)) {
+    return true;
+  } else {
+    return false;
+  }
 
-function validar_cpf(cpf) {
-  var regex = /^(\d)\1{10}$/;
-  return regex.test(cpf);
 }
 
 function validar_telefone(telefone){
