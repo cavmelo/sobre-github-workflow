b2bdcbb0ba045a96262fa234894d65452b632f1e
Seu Nome Aqui <evoney.mendonca@itbam.org.br>
2023-04-27 20:19:45 -0400

    validator python

--- src/validador.py
+++ src/validador.py
@@ -1,16 +1,23 @@
 # Conjunto de REGEX para validação de entradas.
+import re
 
 def validar_email(email):
-  # escreva seu código aqui
+  regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+  return re.match(regex,email,flags=0)  
   
 def validar_cep(cep):
   # escreva seu código aqui
+  return re.match('([0-9]{5}[-]?[0-9]{3})',cep)
   
 def validar_cpf(cpf):
   # escreva seu código aqui
   
-def validar_telefone(telefone):
-  # escreve seu código aqui
+  return re.match('([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})',cpf,flags=0) 
   
+def validar_telefone(telefone):
+  regex = re.compile(r'^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$')
+  # escreve seu código aqui
+  return re.match(regex,telefone)
  
   
