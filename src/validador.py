adbbfcb7a9c32b387e77d076c513ad929589c343
Gabriel Amoêdo <75450592+amoedogabriel@users.noreply.github.com>
2023-04-27 20:39:39 -0400

    Adicionador validadores em Python e Js

--- src/validador.py
+++ src/validador.py
@@ -1,16 +1,35 @@
 # Conjunto de REGEX para validação de entradas.
 
 def validar_email(email):
-  # escreva seu código aqui
+    """Valida se o email é válido usando regex."""
+    regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if re.match(regex, email):
+        return True
+    else:
+        return False
   
 def validar_cep(cep):
-  # escreva seu código aqui
+    """Valida se o CEP é válido usando regex."""
+    regex = r'^\d{5}-?\d{3}$'
+    if re.match(regex, cep):
+        return True
+    else:
+        return False
   
 def validar_cpf(cpf):
-  # escreva seu código aqui
+    """Valida se o CPF é válido usando regex."""
+    regex = r'^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$'
+    if re.match(regex, cpf):
+        return True
+    else:
+        return False
   
 def validar_telefone(telefone):
-  # escreve seu código aqui
-  
+    """Valida se o número de telefone é válido usando regex."""
+    regex = r'^\+?\d{2}\s?\(?\d{2}\)?\s?\d{4,5}-?\d{4}$'
+    if re.match(regex, telefone):
+        return True
+    else:
+        return False
  
   
