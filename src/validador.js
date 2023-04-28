f9d684bf1b83d91c0c8eaf5a357211e6c97cec1f
Josias Cavalcante <jbfc@icomp.ufam.edu.br>
2023-04-27 20:12:25 -0400

    Validador Javascript para simular conflitos

--- src/validador.js
+++ src/validador.js
@@ -3,20 +3,27 @@
 function validar_email(email)
 {
   // escreva seu código aqui
+  // Simulando conflito 1
+  var conflito1 = "";
  
 }
   
 function validar_cep(cep){
   // escreva seu código aqui
-
+  // Simulando conflito 2
+  var conflito2 = "";
 }
 
 function validar_cpf(cpf){
-// escreva seu código aqui
+  // escreva seu código aqui
+  // Simulando conflito 3
+  var conflito3 = "";
 
 }
 
 function validar_telefone(telefone){
   // escreve seu código aqui
+  // Simulando conflito 4
+  var conflito4 = "";
 
 }
