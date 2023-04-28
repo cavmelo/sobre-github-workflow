8efc4ed65bf50fce5d6c2173bbc03daaf86316ff
EMiltonDeveloper <emilton.sistemas@gmail.com>
2023-04-28 11:04:02 -0400

    close #2, closes #3, closes #4, closes #5; Adicionadas validações com regex

--- src/validador.js
+++ src/validador.js
@@ -1,22 +1,21 @@
 // Conjunto de REGEX para validação de entradas.
 
-function validar_email(email)
-{
-  // escreva seu código aqui
- 
-}
-  
-function validar_cep(cep){
-  // escreva seu código aqui
-
+function validar_email(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
 }
 
-function validar_cpf(cpf){
-// escreva seu código aqui
-
+function validar_cep(cep) {
+  const regex = /^\d{5}-\d{3}$/;
+  return regex.test(cep);
 }
 
-function validar_telefone(telefone){
-  // escreve seu código aqui
+function validar_cpf(cpf) {
+  const regex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+  return regex.test(cpf);
+}
 
+function validar_telefone(telefone) {
+  const regex = /^(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})$/;
+  return regex.test(telefone);
 }
