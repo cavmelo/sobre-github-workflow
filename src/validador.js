385631f10195c20311cf0fe1a99be40bddd54ff0
FerreiraBen <jbfc@icomp.ufam.edu.br>
2023-04-28 06:09:34 -0400

    mnhehne

--- src/validador.js
+++ src/validador.js
@@ -1,7 +1,7 @@
 //   Conjunto de REGEX para validação de entradas.
 
-function validar_email(email)
-{
+
+function validar_email(email){
   // escreva seu código aqui
 	var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return regex.test(email.trim().toLowerCase());
